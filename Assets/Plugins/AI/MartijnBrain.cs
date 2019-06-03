aa69fecce79c0b72ef81c278249778b501f4d0cd
Martijn <martijnbouius@gmail.com>
2019-06-03 20:44:40 +0200

    Update MartijnBrain.cs
    -updated my brain

--- Assets/Plugins/AI/MartijnBrain.cs
+++ Assets/Plugins/AI/MartijnBrain.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [CreateAssetMenu(fileName = "Martijn Brain AI", menuName = "Brains/Martijn Brain AI")]
 public class MartijnBrain : BrainBase
@@ -20,5 +21,27 @@ public class MartijnBrain : BrainBase
             public Action<Target> LookAway;
             public Action<Target> BackOff;
          */
+        lastData = data;
+        //lastData.ThrustForward(1);
+        lastData.targets.OrderBy(t => t.health);
+
+        if (lastData.targets[0].position == lastData.me.position)
+        {
+            lastData.LookAt(lastData.targets[1]);
+        }
+
+        else if  (lastData.targets[0].position != lastData.me.position)
+        {
+            lastData.LookAt(lastData.targets[0]);
+        }
+            
+
+         if (lastData.me.health < 25)
+        {
+            lastData.BackOff(lastData.targets[0]);
+        }
+
     }
+
+
 }
