5d5cb00b1d0eb8b73c7e24627ca448950416e702
Petar07 <p.petrov9898@gmail.com>
2019-06-04 13:00:32 +0200

    Update PetarBrain.cs

--- Assets/Plugins/AI/PetarBrain.cs
+++ Assets/Plugins/AI/PetarBrain.cs
@@ -7,9 +7,91 @@ public class PetarBrain : BrainBase
 {
     private BrainData lastData;
 
-    public override void UpdateData(BrainData data)
+	
+
+
+	public override void UpdateData(BrainData data)
     {
-        /*
+		lastData = data;
+
+		int enemy = 0;
+		int currentTime = 0;
+		int randomTime = Random.Range(15,40);
+
+		int timeToSwitch = 20 + currentTime;
+
+
+		void StartLookingForEnemy()
+		{	
+			
+			lastData.LookAt(lastData.targets[enemy]);
+		}
+
+
+		
+
+
+		if (Vector3.Distance(data.me.position, data.targets[enemy].position) > 4f)
+		{
+			lastData.MoveTo(lastData.targets[enemy]);
+		} else if (Vector3.Distance(data.me.position, data.targets[enemy].position) < 4f)
+		{
+			//lastData.BackOff(lastData.targets[enemy]);
+			lastData.LookAway(lastData.targets[enemy]);
+			lastData.ThrustForward(3);
+		}
+
+		
+		
+
+		if (Time.time >= timeToSwitch)
+		{
+			enemy = enemy + 1;
+			currentTime = (int) Time.time;
+			lastData.LookAt(lastData.targets[enemy]);
+			
+		}
+
+		Debug.Log(enemy);
+		Debug.Log(timeToSwitch);
+		Debug.Log(currentTime);
+
+
+		//Debug.Log(timeToSwitch);
+
+
+
+
+
+
+
+
+
+
+		//if (Vector3.Distance(data.me.position, data.targets[enemy].position) > 8f)
+		//{
+		//	if (enemy < 1)
+		//	{
+		//		enemy = enemy + 1;
+		//		ChooseTarget();
+		//	} else if (enemy == 2)
+		//	{
+		//		enemy = 1;
+		//		ChooseTarget();
+		//	}
+
+
+		//}
+
+
+
+
+
+
+
+
+
+		/*
          * Actions Avaliable:
          * 
            
@@ -20,5 +102,5 @@ public class PetarBrain : BrainBase
             public Action<Target> LookAway;
             public Action<Target> BackOff;
          */
-    }
+	}
 }
