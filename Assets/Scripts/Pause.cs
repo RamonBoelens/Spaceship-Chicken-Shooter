2f6ad49f8314a17f4994507d8bfac416603aae1a
Petar07 <p.petrov9898@gmail.com>
2019-06-05 13:02:14 +0200

    Pause Button and Functionality

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+	public GameObject pauseText;
+
+	bool isPaused = false;
+
+
+	public void OnButtonPress()
+	{
+		
+
+
+		// If game is paused, unpause the game
+		if (isPaused)
+		{
+			Time.timeScale = 1.0f;
+			isPaused = false;
+			pauseText.SetActive(false);
+		}
+
+		//The game gets paused
+		else
+		{
+			Time.timeScale = 0.0f;
+			isPaused = true;
+			pauseText.SetActive(true);
+		}
+	}
+}
